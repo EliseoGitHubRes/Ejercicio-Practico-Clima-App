9b922087f8f8253afeddd26fe7db64a3a901f65d
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-26 04:51:38 -0600

    app clima terminada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,262 @@
-import Image from "next/image";
+'use client' // Indica que este componente se ejecuta del lado del cliente (en el navegador).
+//importaciones
+import Navbar from "@/components/NavBar";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
+import { es } from 'date-fns/locale';
+import { useQuery } from "react-query";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
+import { translateWeatherDescription } from "@/utils/translateWeatherDescription";
+import WeatherDetails from "@/components/WeatherDetails";
+import { convertWindSpeed } from "@/utils/convertWindSpeed";
+import "./globals.css";
+import ForecastWeatherDetail from "@/components/ForecastWeatherDetail";
+import Container from "@/components/Container";
+import WeatherIcon from "@/components/WeatherIcon";
+import { loadingCityAtom, placeAtom } from "./atom";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+
+
+// Definición de los tipos de datos para la respuesta de la API
+type WeatherData = {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: Array<{
+    dt: number;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+    };
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    clouds: {
+      all: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+    };
+    visibility: number;
+    pop: number;
+    sys: {
+      pod: string;
+    };
+    dt_txt: string;
+  }>;
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // Usa el átomo llamado placeAtom para obtener su valor actual
+  // La variable 'place' contendrá ese valor
+  const [place] = useAtom(placeAtom);
+  // Usa el átomo llamado loadingCityAtom para obtener su valor actual
+  // La variable 'loadingCity' contendrá ese valor
+  const [loadingCity] = useAtom(loadingCityAtom);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // Consulta de datos de clima usando react-query
+  const { isLoading, data, refetch } = useQuery<WeatherData>('repoData', async () => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_CLIMA_KEY}&cnt=56`);
+    return data;
+  }
+  );
+
+  // Refrescar datos cuando cambie la ciudad
+  useEffect(() => {
+    refetch();
+  }, [place, refetch])
+
+  // Asigna el primer elemento de la lista 'list' dentro del objeto 'data' a la variable 'firstData'
+  // Usa el operador de encadenamiento opcional (?.) para evitar errores si 'data' es null o undefined
+  const firstData = data?.list[0];
+
+  // Imprime el valor de 'data' en la consola para depuración
+  //console.log("data", data);
+
+
+  // Obtener fechas únicas de las previsiones
+  const uniqueDates = [
+    ...new Set(
+      data?.list.map((entry) =>
+        new Date(entry.dt * 1000).toISOString().split("T")[0]
+      )
+    )
+  ].slice(0, 5); // Limitar a los primeros 5 días
+
+  // Filtrar la primera entrada disponible para cada día
+  const firstDataForEachDate = uniqueDates.map((date) => {
+    return data?.list.find((entry) => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
+      return entryDate === date;
+    });
+  });
+
+  // Mostrar un mensaje de carga mientras se obtienen los datos
+  if (isLoading) return (
+    <div className="flex items-center min-h-screen justify-center">
+      <p className="animate-bounce">Cargando...</p>
     </div>
+  )
+
+  return (
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
+      <Navbar location={data?.city.name} />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4 ">
+        {/* DATOS DE HOY  */}
+        {loadingCity ? (
+          <WeatherSkeleton />
+        ) : (
+          <>
+            <section className="space-y-4">
+              <div className="space-y-2">
+                <h2 className="flex gap-1 text-2xl  items-end">
+                  <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE", { locale: es })
+                    .charAt(0).toUpperCase() +
+                    format(parseISO(firstData?.dt_txt ?? ""), "EEEE", { locale: es }).slice(1)}
+                  </p>
+                  <p className="text-lg">
+                    ({format(parseISO(firstData?.dt_txt ?? ""), "dd-MM-yyyy")})
+                  </p>
+                </h2>
+                <div className="flex gap-4">
+                  {/* Contenedor para la temperatura  */}
+                  <Container className=" bg-white shadow-lg px-6 py-4 gap-2 justify-center items-center w-full max-w-md mx-auto rounded-lg">
+                    {/* Temperatura */}
+                    <div className="w-full flex flex-col justify-center gap-2 items-center text-xs font-semibold  ">
+                      <p className="whitespace-nowrap">
+                        <span>Temperatura</span>
+                      </p>
+                      <p className="text-5xl whitespace-nowrap">
+                        <span >
+                          {convertKelvinToCelsius(firstData?.main.temp ?? 296.37)}°C
+                        </span>
+                      </p>
+                      <p className="whitespace-nowrap">
+                        <span>
+                          {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}
+                          °↓{" "}
+                        </span>
+                        <span>
+                          {" "}
+                          {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}
+                          °↑
+                        </span>
+                      </p>
+                    </div>
+                    <div className="w-full flex flex-col justify-center gap-2 items-center text-xs font-semibold ">
+                      <p className="whitespace-nowrap">
+                        {translateWeatherDescription(firstData?.weather[0].description ?? "")}{" "}
+                      </p>
+                      <WeatherIcon
+                        iconName={getDayOrNightIcon(
+                          firstData?.weather[0].icon ?? "",
+                          firstData?.dt_txt ?? ""
+                        )}
+                      />
+                    </div>
+                  {/* Contenedor para mostrar detalles del clima*/}
+                  </Container>
+                  <Container className=" bg-sky-400/80  px-6 gap-2 justify-center max-w-screen-md mx-auto">
+                    <WeatherDetails
+                      humidity={`${firstData?.main.humidity}%`}
+                      windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)} />
+                      {/* Muestra la humedad y la velocidad del viento, 
+                        utilizando valores por defecto si no hay datos. */}
+                  </Container>
+                </div>
+              </div>
+            </section>
+
+            {/* DATOS LOS SIGUENTES 5 DIAS */}
+            <section className="flex w-full flex-col gap-4">
+              <p className="text-2xl">Pronóstico (5 días)</p>
+              {firstDataForEachDate.map((d, i) => (
+                // Mapea cada elemento en el array firstDataForEachDate,
+                <ForecastWeatherDetail
+                  key={i} // Clave única para identificar cada componente en el array, utilizando el índice.
+                  description={translateWeatherDescription(d?.weather[0].description ?? "")}
+                  weatherIcon={d?.weather[0].icon ?? "01d"}
+                  date={format(parseISO(d?.dt_txt ?? ""), "dd-MM")}
+                  feels_like={d?.main.feels_like ?? 0}//muestra la temperatura que se siente del dia
+                  temp={d?.main.temp ?? 0}//obtiente la temperatura actual
+                  temp_max={d?.main.temp_max ?? 0}
+                  temp_min={d?.main.temp_min ?? 0}
+                  humidity={`${d?.main.humidity}% `}
+                  windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)} `}
+                />
+              ))}
+            </section>
+          </>)}
+      </main>
+    </div>
+  )
+};
+
+// Componente de Skeleton para cargar
+function WeatherSkeleton() {
+  return (
+    <section className="space-y-8 ">
+      {/*skeleton de datos de hoy  */}
+      <div className="space-y-2 animate-pulse">
+        {/* fecha skeleton */}
+        <div className="flex gap-1 text-2xl items-end ">
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+        </div>
+
+        {/* skeleton de datos de la temperatura */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {[1, 2, 3, 4].map((index) => (
+            <div key={index} className="flex flex-col items-center space-y-2">
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* skeleton de pronostico de los siguientes 5 dias*/}
+      <div className="flex flex-col gap-4 animate-pulse">
+        <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
+
+        {[1, 2, 3, 4, 5].map((index) => (
+          <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
